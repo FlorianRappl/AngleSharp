bb75825105228461c9ae879334da3869112bb188
Florian Rappl <rappl.florian@gmail.com>
2016-11-24 21:59:59 +0100

    Added test to verify #457

---
+++ src/AngleSharp.Core.Tests/Xml/XmlDom.cs
@@ -0,0 +1,21 @@
+﻿namespace AngleSharp.Core.Tests.Xml
+{
+    using AngleSharp.Xml.Parser;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class XmlDomTests
+    {
+        [Test]
+        public void XmlManipulationUsingInnerHtmlShouldWork()
+        {
+            var parser = new XmlParser();
+            var document = parser.ParseDocument("<div></div>");
+            var div = document.QuerySelector("div");
+
+            div.InnerHtml = "<a></a>";
+
+            Assert.AreEqual("<div><a></a></div>", div.OuterHtml);
+        }
+    }
+}
